c6eb5e6d3e15e67a02d2d15ee348340531100651
amartelr <alfredo.martel@gmail.com>
2020-06-10 19:20:39 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module oracle_go
+
+go 1.14
+
+require github.com/godror/godror v0.16.1
---
+++ oracle_go.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/godror/godror"
+)
+
+func main() {
+	db, err := sql.Open("godror", "system/pass@localhost:1521/xe")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select sysdate from dual")
+	if err != nil {
+		fmt.Println("Error running query")
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+
+	var thedate string
+	for rows.Next() {
+		rows.Scan(&thedate)
+	}
+	fmt.Printf("The date is: %s\n", thedate)
+}
